b955a3df2d38b5e605658954b07189f7fa085750
Veronica Zhukova <veronicazhukova@MacBook-Air-Veronica.local>
2022-11-27 00:15:05 +0200

    added task 3

---
+++ 3-squareSum.js
@@ -0,0 +1,13 @@
+function squareSum (arr) {
+  let sum = 0;
+
+  for (let i = 0; i < arr.length; i++) {
+    sum += arr[i] * arr[i]
+  }
+
+  return sum;
+}
+
+const arr = [1, 2, 3, 4, 5]
+
+console.log(squareSum(arr))
